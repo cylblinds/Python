33afa214a9e4d6f244442a65477a0ce2626f3046
Mark <31583038+cylblinds@users.noreply.github.com>
2019-01-20 01:16:58 +0800

    Create TextProcessbar.py
    文本进度条

---
+++ TextProcessbar.py
@@ -0,0 +1,14 @@
+import time
+
+scale = 50
+print('执行开始'.center(scale//2,'-'))
+start = time.perf_counter()
+for i in range(scale+1):
+    a = '*' * i
+    b = '.' * (scale - i)
+    c = i/scale * 100
+    dur = time.perf_counter() - start
+    print('\r{:^3.0f}%[{}-->{}]{:.2f}s'.format(c,a,b,dur),end = '')   #\r表示光标移到行首，end = ''表示光标不换行
+    time.sleep(0.2)
+
+print('\n' + '执行结束'.center(scale//2,'-'))
